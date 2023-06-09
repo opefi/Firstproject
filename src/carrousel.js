f700f495c8dd37980a4a7ac83f1f673572ab1b00
opefi <pierre.francois.filippi@gmail.com>
2023-06-09 15:59:24 +0200

    make it betteryjhnhhnjkghgdffhdklj

--- src/carrousel.js
+++ src/carrousel.js
@@ -1,13 +1,11 @@
 /** @format */
 
 import Carousel from "react-bootstrap/Carousel";
-import paysage from "./paysage.jpg";
 
 function UncontrolledExample() {
 	return (
 		<Carousel>
 			<Carousel.Item>
-				<img className="d-block w-100" src={paysage} alt="First slide" />
 				<Carousel.Caption>
 					<h3>First slide label</h3>
 					<p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
@@ -20,12 +18,6 @@ function UncontrolledExample() {
 				</Carousel.Caption>
 			</Carousel.Item>
 			<Carousel.Item>
-				<img
-					className="d-block w-100"
-					src={require("./photosgite/vetta1/Vetta2A.jpg")}
-					alt="Third slide"
-				/>
-
 				<Carousel.Caption>
 					<h3>Third slide label</h3>
 					<p>
